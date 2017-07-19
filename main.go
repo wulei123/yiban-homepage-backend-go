ec8a372e4d744ffbb5b77ce4cc58c3101014cbcc
wulei123 <wulei153@qq.com>
2017-07-19 15:59:06 +0800

    finish single file upload

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"os"
 	"time"
+	"io"
 )
 var Data_Origin_Path = configuration.DataUrl + "data_copy.json"
 type NoticesStruct struct{
@@ -89,8 +90,10 @@ func CheckLogin(w http.ResponseWriter, r *http.Request){
 	//println(session.Values["login"].(bool))
 	if sess.Values["login"] == true{
 		fmt.Fprint(w,`{"code":1,"message":"你已经登陆"}`)
+		return
 	}else{
 		fmt.Fprint(w,`{"code":2,"message":"你还没有登陆"}`)
+		return
 	}
 }
 
@@ -104,50 +107,89 @@ func pathExists(path string) (bool, error) {
 	}
 	return false, err
 }
+func checkLogin(w http.ResponseWriter, r *http.Request)(login bool, err error){
+	sess, err := store.Get(r,"JsSession")
+	if err != nil{
+		log.Printf("session error : %v \n",err)
+		return false, err
+	}
+	if sess.Values["login"] == true{
+		return true,err
+	}else{
+		fmt.Fprint(w,`{"code":2,"message":"你还没有登陆"}`)
+		return false,err
+	}
+	return false,err
+}
+
 
 func FormSubmit(w http.ResponseWriter, r *http.Request){
 	allowCORS(w)
-	CheckLogin(w,r)
-	decoder := json.NewDecoder(r.Body)
-	var data Data
-	err := decoder.Decode(&data)
-	if err != nil{
-		fmt.Fprintf(w,`{"code":4,"message":"解析失败","error":%v}`,err)
-	}
-	fmt.Println(data)
-	bd,err := json.Marshal(data)
-	if err != nil{
-		log.Printf("save failed : %v\n",err)
-		return
-	}
-	exist, err := pathExists(Data_Origin_Path)
-	if exist{
-		newPath := fmt.Sprintf("%s%v_data.json",configuration.DataUrl,time.Now().Format("2006-01-02_15_04_05"))
-		err = os.Rename(Data_Origin_Path,newPath)
+	if login,_ := checkLogin(w,r);login == true{
+		decoder := json.NewDecoder(r.Body)
+		var data Data
+		err := decoder.Decode(&data)
+		if err != nil{
+			fmt.Fprintf(w,`{"code":4,"message":"解析失败","error":%v}`,err)
+		}
+		fmt.Println(data)
+		bd,err := json.Marshal(data)
 		if err != nil{
 			log.Printf("save failed : %v\n",err)
 			return
 		}
-	}
+		exist, err := pathExists(Data_Origin_Path)
+		if exist{
+			newPath := fmt.Sprintf("%s%v_data.json",configuration.DataUrl,time.Now().Format("2006-01-02_15_04_05"))
+			err = os.Rename(Data_Origin_Path,newPath)
+			if err != nil{
+				log.Printf("save failed : %v\n",err)
+				return
+			}
+		}
 
-	df, err := os.OpenFile(Data_Origin_Path, os.O_WRONLY|os.O_CREATE, 0666)
-	if err != nil{
-		log.Printf("save failed : %v\n",err)
-		return
-	}
-	defer df.Close()
-	_ , err = df.Write(bd)
-	if err != nil{
-		log.Printf("save failed : %v\n",err)
-		return
+		df, err := os.OpenFile(Data_Origin_Path, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil{
+			log.Printf("save failed : %v\n",err)
+			return
+		}
+		defer df.Close()
+		_ , err = df.Write(bd)
+		if err != nil{
+			log.Printf("save failed : %v\n",err)
+			return
+		}
+		fmt.Fprint(w,`{"code":5,"message":"上传成功,数据已存储"}`)
 	}
 }
-
+func HandleFileUpload(w http.ResponseWriter, r *http.Request){
+	allowCORS(w)
+	if login,_ := checkLogin(w,r); login == true{
+		r.ParseMultipartForm(32 << 20)
+		file, handler , err := r.FormFile("icon")
+		if err != nil{
+			log.Println(err)
+			return
+		}
+		defer file.Close()
+		//fmt.Fprintf(w, "%v", handler.Header)
+		newFileName := time.Now().Format("2006-01-02_15_04_05")+handler.Filename
+		f, err := os.OpenFile(configuration.ImageUrl+newFileName, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil{
+			fmt.Printf("打开文件失败 : %v\n",err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f,file)
+		fmt.Fprintf(w,`{"code":6,"message":"图片上传成功","image_url":"%s"}`,newFileName)
+	}
+}
 func main(){
 
 	http.HandleFunc("/homepage/login",Login)
 	http.HandleFunc("/homepage/login/check",CheckLogin)
 	http.HandleFunc("/homepage/form",FormSubmit)
+	http.HandleFunc("/homepage/image",HandleFileUpload)
 	//http.HandleFunc("/test/cookie",testCookie)
 	//http.HandleFunc("/test/session",testSession)
 	log.Printf("启动服务 : localhost%s\n",configuration.ServerPort)
